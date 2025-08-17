4cf20679c3d9df37694b2bdb537c228c01dbf348
mj163 <marti@rdolcegroup.com>
2025-08-17 16:53:30 -0500

    chore: update gitignore files and add environment variable configurations

--- env.mjs
+++ env.mjs
@@ -1,6 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
-
+// Use the .env root as the global environment file for all apps and packages
 export const env = createEnv({
   server: {
     GH_TOKEN: z.string().min(1),
@@ -11,6 +11,11 @@ export const env = createEnv({
     NODE_ENV: z.string().min(1)
   },
   runtimeEnv: {
+    GH_TOKEN: process.env.GH_TOKEN,
+    GH_URL: process.env.GH_URL,
+    GH_SSH: process.env.GH_SSH,
+    DATABASE_TYPE: process.env.DATABASE_TYPE,
+    DATABASE_URI: process.env.DATABASE_URI,
     NODE_ENV: process.env.NODE_ENV
-  },
+  }
 })
